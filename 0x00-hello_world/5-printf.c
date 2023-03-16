aa95464b130a13bec57253593eb2ea105d8a121d
Ademola Aina <ademic.aina@gmail.com>
2023-03-16 13:16:33 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ * prints Programing using printf
+ * to the standard output
+ */
+int main(void)
+{
+printf("with proper gramar, but the outcome is a piece of art, \n");
+return (0);
+}
