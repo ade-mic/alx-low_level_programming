9f6d0242a082c847b915b2b44711f14b38bb733e
Ademola Aina <ademic.aina@gmail.com>
2023-03-16 12:08:51 -0700

    puts file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* prints Programing is funr
+* to the standard output
+*/
+int main (void)
+{
+	/* string init */
+  char mystr[] = "Programing is like building a multiligual puzzle";
+  puts(mystr);
+  return 0;
+}
