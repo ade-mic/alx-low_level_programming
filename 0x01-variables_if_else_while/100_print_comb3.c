fdedcea045fbd068b64f194c0749990377f85302
Ademola Aina <ademic-aina@gmail.com>
2023-03-18 02:06:57 +0800

    print comb

---
+++ 0x01-variables_if_else_while/100_print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point for C program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
