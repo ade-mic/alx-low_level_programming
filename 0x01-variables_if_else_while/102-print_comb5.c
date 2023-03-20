8c577cd14eb96feea1f5af897ae0015805fab25c
Ademola Aina <ademic-aina@gmail.com>
2023-03-20 14:44:16 +0800

    trying to seolve

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,19 +12,22 @@ int main(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			for (k = 0; k <= 9; k++)
 			{
-				for (l = k + 1; l <= 9; l++)
+				for (l = j + 1; l <= 9; l++)
 				{
 					putchar(i + '0');
 					putchar(j + '0');
 					putchar(' ');
 					putchar(k + '0');
 					putchar(l + '0');
-					putchar(',');
-					putchar(' ');
+					if (i < 10 && j < 9 &&)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}
