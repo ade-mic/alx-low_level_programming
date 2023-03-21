05a84f0ce81ef6b1bb0f139f76b435b783dc6284
Ademola Aina <ademic-aina@gmail.com>
2023-03-21 21:22:23 +0800

    progress 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,4 @@
-#include <stdio.h>
-void print_alphabet(void);
-/**
- * main - Program Entrance
- * Return: Always return a value of 0 if successful
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
+#include "main.h"
 /**
  * print_alphabet - print the alphabet in lower case
  * Return: void
@@ -19,7 +8,7 @@ void print_alphabet(void)
 	char alpha;
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
-		putchar(alpha);
+		_putchar(alpha);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
-#ifindef MAIN_H_
+#ifndef MAIN_H_
 #define MAIN_H_
 
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif
