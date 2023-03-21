90f9595ae638322dcd3583c11401056be3556ea9
Ademola Aina <ademic-aina@gmail.com>
2023-03-22 02:41:32 +0800

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * add - add 2 inegers
+ *@i: first integer value
+ *@j: second integer value
+ *
+ * Return: the result of the addition
+ *
+ */
+int add(int i, int j)
+{
+	int add_val = i + j;
+
+	return (add_val);
+}
