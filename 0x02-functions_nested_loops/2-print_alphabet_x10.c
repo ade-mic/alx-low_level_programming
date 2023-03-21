007fff5cfdbdaf0cf6bf7104504bf0191f9702f9
Ademola Aina <ademic-aina@gmail.com>
2023-03-21 21:42:46 +0800

    exercise 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,29 +1,20 @@
-#include <stdio.h>
-void print_alphabet_x10(void);
-/**
- * main - Program Entrance
- * Return: Always return a value of 0 if successful
- */
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
+#include "main.h"
 
 /**
- * print_alphabet - print the alphabet in lower case
+ * print_alphabet_x10 - print the alphabet in lower case
  * Return: void
  */
 void print_alphabet_x10(void)
 {
 	char alpha;
 	int i;
+
 	for (i = 0; i < 10; i++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
-			for (alpha = 'a'; alpha <= 'z'; alpha++)
-			{
-				putchar(alpha);
-			}
-			putchar('\n');
+			_putchar(alpha);
 		}
+		_putchar('\n');
+	}
 }
