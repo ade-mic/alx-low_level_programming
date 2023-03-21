a2135f2777ac357c987275c8e59d825c7a7a91c1
Ademola Aina <ademic-aina@gmail.com>
2023-03-22 01:07:57 +0800

    24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 /**
  * jack_bauer - print every minute of the day of Jack Bauer
- *
+ * Return: void
  */
 void jack_bauer(void)
 {
 	int i, j, k, l;
 
-	for (i = 0; i <=2; i++)
+	for (i = 0; i <= 2; i++)
 	{
-		for (j = 0; j < 4; j++)
+		for (j = 0; j < 10; j++)
 		{
+			if (i == 2 && j == 4)
+				break;
 			for (k = 0; k < 6; k++)
 			{
 				for (l = 0; l < 10; l++)
