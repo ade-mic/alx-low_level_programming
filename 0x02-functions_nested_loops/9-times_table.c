06391db1be00dfd15a2fe7159b5d56406fc14c1c
Ademola Aina <ademic-aina@gmail.com>
2023-03-22 02:02:21 +0800

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * times_table - print 9 times table
+ * Return: void
+ */
+void times_table(void)
+{
+	int i, j, val;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			val = (i + 0) * (j + 0);
+			if (val / 10 == 0)
+				_putchar(' ');
+			else
+				_putchar('0' + val / 10);
+			_putchar('0' + val % 10);
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
