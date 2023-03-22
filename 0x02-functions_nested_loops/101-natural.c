a1c0f1d299e78488ea85aee4783d9666fef9edee
Ademola Aina <ademic-aina@gmail.com>
2023-03-22 16:47:11 +0800

    natural number sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+void print(long i);
+/**
+ * natural_numbers - print sum of natural numbers below 1024
+ *
+ *
+ * Return: void
+ *
+ */
+void natural_numbers(void)
+{
+	int sum, i;
+
+	sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+	print(sum);
+	_putchar('\n');
+}
+/**
+ * print - recursively print input
+ *@i: value to be printed
+ *
+ * Return: void
+ *
+ */
+
+void print(long i)
+{
+	/* If number is smaller than 0, put a sign */
+	/* and change number to positive */
+	if (i < 0)
+	{
+		_putchar('-');
+		i = -i;
+	}
+		/* Remove the last digit aand recur */
+	if (i / 10)
+		print(i / 10);
+	/* Print the last digit */
+	_putchar(i % 10 + '0');
+}
