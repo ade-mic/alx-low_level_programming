1733081f7d293c05841dad50ede476a5e52156c2
Ademola Aina <ademic-aina@gmail.com>
2023-03-23 23:38:51 +0800

    def

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+
+/**
+ * mul - multiply a and b
+ *
+ * @a: first integer
+ * @b: the second integer
+ * Return: 1 if c uppercase
+ *
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
