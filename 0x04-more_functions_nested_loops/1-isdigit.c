5bbeae738fff8cf96f05b0bca0f42cf22a0805ff
Ademola Aina <ademic-aina@gmail.com>
2023-03-23 23:19:32 +0800

    more

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * @c: character check
+ * Return: 1 if c uppercase
+ *
+ */
+
+int _isdigit(int c)
+{
+	if (isdigit(c))
+		return (1);
+	else
+		return (0);
+}
