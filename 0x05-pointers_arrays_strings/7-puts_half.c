89b14bccb933f008e8d9022fbed8f81173ab9d03
Ademola Aina <ademic-aina@gmail.com>
2023-03-28 10:35:03 +0800

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * puts_half - prints everyother character of a string
+ *
+ * @s: string to be reversed
+ *
+ * Return: void
+ */
+
+void puts_half(char *s)
+{
+	int len, i, k;
+	char *tmp;
+
+	len = strlen(s);
+	tmp = s;
+	k = len;
+	i = k / 2;
+	while (i <= k)
+	{
+		_putchar(tmp[i]);
+		i++;
+	}
+	_putchar('\n');
+}
