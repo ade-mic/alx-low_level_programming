d3642eaaba2a8b8d1322868efcb7b1b52301f5e8
Ademola Aina <ademic-aina@gmail.com>
2023-03-28 09:41:58 +0800

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - reverse string
+ *
+ * @s: string to be reversed
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int len, i, k;
+	char tmp;
+
+	len = strlen(s);
+	/* reverse */
+	i = 0;
+	k = len - 1;
+	while (i < (len/2))
+	{
+		tmp = s[k];
+		s[k] = s[i];
+		s[i] = tmp;
+		i++;
+		k--;
+	}
+}
