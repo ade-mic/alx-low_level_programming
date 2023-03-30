7e4e979dc4fbfaa1bfa0b4e08a61bf9685f71bd3
Ademola Aina <ademic-aina@gmail.com>
2023-03-30 09:37:38 +0800

    cap sting

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -16,4 +16,7 @@ char *_strcat(char *dest, char *str);
 char *_strncat(char *dest, char *str, int n);
 char *_strncpy(char *dest, char *str, int n);
 int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
 #endif
