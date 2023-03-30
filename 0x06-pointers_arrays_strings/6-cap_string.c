a592f3d7010c79aa565f9e35b4456c55f4acd9bb
Ademola Aina <ademic-aina@gmail.com>
2023-03-30 09:25:17 +0800

    capitalize world

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * *cap_string - capitalizes all words of a string
+ *
+ * @s: string
+ *
+ *
+ * Return: capitalize sting every word
+ */
+
+char *cap_string(char *s)
+{
+	int len, i;
+	char *tmp;
+
+	tmp = s;
+	len = strlen(s);
+
+	for (i = 0; i < len; i++)
+	{
+		if (isspace(tmp[i]) || tmp[i] == ',' || tmp[i] == ';'	\
+		    || tmp[i] == '.' || tmp[i] == '!' || tmp[i] == '?' || \
+		    tmp[i] == '"' || tmp[i] == '(' || tmp[i] == ')' || \
+		    tmp[i] == '{' || tmp[i] == '}' || tmp[i] == ' ')
+			tmp[i + 1] = toupper(tmp[i + 1]);
+	}
+	return tmp;
+}
