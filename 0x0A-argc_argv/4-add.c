ee8fb4e578d364aa0f61ea838cd494e0fdd8bfdb
Ademola Aina <ademic-aina@gmail.com>
2023-04-10 11:38:28 +0800

    add me

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - Entry to the C program
+ * @argc: the size of arguements array
+ * @argv: arguement array
+ * Return: 0 for success and any other digit for error
+ */
+int main(int argc, char *argv[])
+{
+	int i, add;
+
+	add = 0;
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (!(atoi(argv[i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				add += atoi(argv[i]);
+			}
+		}
+		printf("%d\n", add);
+	}
+	return (0);
+}
