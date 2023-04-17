067e8ad0f35eb2c9ffd77508d97064d0b4c3a173
Ademola Aina <ademic-aina@gmail.com>
2023-04-18 06:10:46 +0800

    struct 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * init_dog - initialize struct dog
+ * @d: variable name
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner's name
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
+}
