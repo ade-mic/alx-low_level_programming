02cf1f27d4bb3f874671c5d15ba7ec75fe56c8e6
Ademola Aina <ademic-aina@gmail.com>
2023-06-16 15:49:57 +0800

    concat str

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,7 +10,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *mem;
-	unsigned int n1, n2, i;
+	unsigned int n1, n2, i, j;
 
 	/* check string */
 	if (s1 == NULL)
@@ -19,27 +19,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	/* length of each string */
 	n1 = strlen(s1);
-	n2 = n;
-	if (n >= strlen(s2))
-		n2 = strlen(s2);
+	n2 = strlen(s2);
+	if (n >= n2)
+		n = n2;
 	/* Allocate memory */
-	mem = malloc((n1 + n2) * sizeof(char));
+	mem = malloc((n1 * sizeof(char) + n1 * sizeof(char) + 1));
 	if (!mem)
 		return (NULL);
 	for (i = 0; i <= n1; i++)
 	{
+		mem[i] = s1[i];
 		/* check for null terminator */
 		if (s1[i] == '\0')
 		{
 			mem[i] = s2[0];
-			break;
+			for (j = 1; j <= n; j++)
+				mem[i + j] = s2[j];
 		}
-		else
-			mem[i] = s1[i];
 	}
-	for  (i = 1; i < n2; i++)
-	{
-		mem[i + n1] = s2[i];
-	}
+	mem[i + j] = '\0';
 	return (mem);
 }
