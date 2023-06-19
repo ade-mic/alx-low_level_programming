eafee9e877808d9484e5931b68d5b9658aee4eb9
Ademola Aina <ademic-aina@gmail.com>
2023-06-19 21:56:08 +0800

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,11 +10,16 @@
 
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+		exit(0);
 	if (d->name == NULL)
 		printf("Name: (nil)");
 	else
 		printf("Name: %s\n", d->name);
-	printf("Age: %.6f\n", d->age);
+	if(!d)
+		printf("Age: (nil)");
+	else
+		printf("Age: %.6f\n", d->age);
 	if (d->owner == NULL)
 		printf("Owner (nil)");
 	else
