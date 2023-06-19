746917dfc1a6cbbb821253f4856b2a8f239743fe
Ademola Aina <ademic-aina@gmail.com>
2023-06-19 17:25:31 +0800

    dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - dog description
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* ifndef _DOG_H_ */
