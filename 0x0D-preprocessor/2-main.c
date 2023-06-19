316473ec29adbb012cbceabac2d2233c9f8ab66e
Ademola Aina <ademic-aina@gmail.com>
2023-06-19 16:04:27 +0800

    preprocessors task 1-4

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main program that prints the name of the file
+ *
+ *Return: 0 for successful compilation
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
