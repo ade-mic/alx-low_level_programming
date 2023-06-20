a291e907287be0da82b2b1eb4f9d8d7d37d6b247
Ademola Aina <ademic-aina@gmail.com>
2023-06-20 10:14:40 +0800

    dog 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
-#include "stdlib.h"
+#include <stdlib.h>
+#include <string.h>
 /**
  * *new_dog - creates new dog
  * @name: name of dog
@@ -10,15 +11,27 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new;
+	char *_name, *_owner;
 
-	if (!name)
+	_name = malloc(strlen(name) + 1);
+	_owner = malloc(strlen(owner) + 1);
+
+	if (!_name || !_owner)
 		return (NULL);
+	/* store a copy of name and owner */
+	strcpy(_name, name);
+	strcpy(_owner, owner);
+	/* allocate memory for new dog data */
 	new = malloc(sizeof(struct dog));
 	if (!new)
+	{
+		free(_name);
+		free(_owner);
 		return (NULL);
+	}
 
-	new->name = name;
+	new->name = _name;
 	new->age = age;
-	new->owner = owner;
+	new->owner = _owner;
 	return (new);
 }
