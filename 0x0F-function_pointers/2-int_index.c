19ffb0a9d999943e850c41fad8230ed30dd904a1
Ademola Aina <ademic-aina@gmail.com>
2023-06-22 08:59:13 +0800

    correction 3

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,14 +13,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, result;
 
-	if (size <= 0)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (array && size && cmp)
 	{
-		result = cmp(array[i]);
-		if (result != 0)
-			return (i);
+		if (size <= 0)
+			return (-1);
+
+		for (i = 0; i < size; i++)
+		{
+			result = cmp(array[i]);
+			if (result != 0)
+				return (i);
+		}
 	}
 	return (-1);
 }
