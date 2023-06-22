aaee545ac55fed97d2b6e81336a130eb064ad179
Ademola Aina <ademic-aina@gmail.com>
2023-06-22 22:29:15 +0800

    print strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - prints string, followed by a new line
+ * @separator: is the string to be printed between the strings
+ * @n: is the number of strings passed to the function
+ *
+ * Return: Nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list arg;
+	char* yield;
+
+	va_start(arg, n);
+	for (i = 0; i < n; ++i)
+	{
+		yield = va_arg(arg, char*);
+		if (yield != NULL)
+			printf("%s", yield);
+		else
+			printf("(nil)");
+
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+	va_end(arg);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,7 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
 int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
