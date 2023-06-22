ad4eaab5b951fc963c9187cf0ed603ae1c6e4b58
Ademola Aina <ademic-aina@gmail.com>
2023-06-22 08:55:31 +0800

    correction 2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,9 +2,9 @@
 /**
  * array_iterator - a function that executes a function given as aparament on
  * on each element of an array
- * array: array
- * size: sice of the array
- * action: function to be executed
+ * @array: array
+ * @size: sice of the array
+ * @action: function to be executed
  *
  * Return: Nothing
  */
@@ -13,6 +13,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (array && size && action)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
 }
