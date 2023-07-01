0a46f2f8bb6bc5e15942924a950641d08b7c2f78
Ademola Aina <ademic-aina@gmail.com>
2023-07-01 20:06:02 +0800

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,6 +2,7 @@
 /**
  * add_node - adds a new node at the beginning of a linked list
  * @str: strings to be added to new list
+ * @head: pointer to head of the linked list
  *
  * Return: the address of the new element or NULL if it failed
  */
