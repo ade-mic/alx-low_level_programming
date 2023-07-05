2fa784ddd010e44bf41468c42bdbe50b2ab742bd
Ademola Aina <ademic-aina@gmail.com>
2023-07-05 10:50:21 +0800

    return nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,13 +8,15 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
-
-	while (head->next != NULL)
+	if (head)
 	{
-		head = head->next;
-		i++;
-		if (i == index)
-			return (head);
+		while (head->next != NULL)
+		{
+			head = head->next;
+			i++;
+			if (i == index)
+				return (head);
+		}
 	}
-	return (head);
+	return (NULL);
 }
