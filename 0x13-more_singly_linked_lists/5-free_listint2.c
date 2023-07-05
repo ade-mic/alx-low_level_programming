f14da45b465ae4ec4983cae7961dd51204c84969
Ademola Aina <ademic-aina@gmail.com>
2023-07-05 08:20:20 +0800

    correction onfreeint 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current;
 
-
-	current = *head;
-	while (*head != NULL)
+	if (head)
 	{
 		current = *head;
-		*head = current->next;
-		free(current);
+		while (*head != NULL)
+		{
+			current = *head;
+			*head = current->next;
+			free(current);
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
