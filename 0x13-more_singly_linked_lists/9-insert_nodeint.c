e4af5c1cab73d741fff2367f23df11df93f65a79
Ademola Aina <ademic-aina@gmail.com>
2023-07-06 04:23:45 +0800

    add node to spec position

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: head node
+ * @idx: index to insert node
+ * @n: value of node to be added
+ * Return: address of the new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	listint_t *new;
+	listint_t *current;
+
+	if (*head)
+	{
+		current = *head;
+		while (current != NULL)
+		{
+			i++;
+			if (i == idx)
+			{
+				new = malloc(sizeof(listint_t));
+				if (!new)
+					return (NULL);
+				new->n = n;
+				new->next = current->next;
+				current->next = new;
+			}
+			else
+				current = current->next;
+
+		}
+		return (new);
+	}
+	return (NULL);
+}
