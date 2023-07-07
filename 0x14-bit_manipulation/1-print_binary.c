455107833116d065c6e6dfde4aa7595a9476da38
Ademola Aina <ademic-aina@gmail.com>
2023-07-07 12:23:53 +0800

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,17 +10,26 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned int mask, bit;
+	unsigned int mask;
+	int skip = 0;
 
-	if (n== 0)
+	if (n == 0)
 		putchar(n + '0');
 	else
 	{
 		mask = 1 << (sizeof(unsigned int) * 8 - 1);
-		while(mask > 0)
+		while (mask > 0)
 		{
-			bit = (mask & n) ? 1 : 0;
-			_putchar(bit + '0');
+			if (n & mask)
+			{
+				skip = 1;
+				_putchar(1 + '0');
+			}
+			else
+			{
+				if (skip)
+					_putchar('0');
+			}
 			mask >>= 1;
 		}
 	}
