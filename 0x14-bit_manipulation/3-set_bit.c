032bbb56e818713abfe23b9f7eed7db288ee0aaf
Ademola Aina <ademic-aina@gmail.com>
2023-07-07 17:02:00 +0800

    set bit check 9

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,6 +3,7 @@
  * set_bit - sets the value of a bit to 1 at a given index
  * @n: integer to set index to 1
  * @index: is the index, starting from 0 of the bit you want to set
+ * Return: new set bit
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
