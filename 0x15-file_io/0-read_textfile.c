50164cc336ea626e6db21c86ba5ebfddb9461b2b
Ademola Aina <ademic-aina@gmail.com>
2023-09-05 07:59:18 +0800

    read_textfiles

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,16 +1,13 @@
 #include "main.h"
 /**
  * read_textfile - a function that reads a textfile and prints it to the POSIX
- * standard output
  * @filename: file to be read
  * @letters: the number of letters it should read nd print
- *
  * Return: the acutal number of letters it should read and print
  **/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t nb_read, tb_read, b_read;
-	ssize_t nb_write; /** function read return value */
+	ssize_t nb_read, tb_read, b_read, nb_write;
 	int fd;    /** file descriptor */
 	char *temp; /** temporarily stored the read character **/
 	int buffsize = ((int) letters + 1) * sizeof(char);
@@ -18,10 +15,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 		return (0);
-
 	temp = malloc(buffsize);
 	if (temp == NULL)
 	{
@@ -29,10 +24,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	tb_read = letters;
 	nb_read = 0;
-	while(nb_read <= tb_read)
+	while (nb_read <= tb_read)
 	{
 		b_read = read(fd, (temp + nb_read), 1);
 		if (b_read == -1)
@@ -45,7 +39,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 			break;
 		nb_read += b_read;
 	}
-
 	temp[nb_read] = '\0';
 	nb_write = write(1, temp, nb_read);
 	if (nb_read != nb_write)
