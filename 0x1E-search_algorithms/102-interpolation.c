b03e8a9660919e08950a47e7b7e342e666726897
ade-mic <ademic.aina@gmail.com>
2024-03-12 18:09:10 +0100

    102-interpolation

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -47,13 +47,11 @@ int interpolation_search(int *array, size_t size, int value)
 		else
 			high = pos - 1;
 	}
-	if (low <= high && value >= array[low] && value <= array[high])
-	{
-		pos = low + (((double)(high - low) / (array[high]
+
+	pos = low + (((double)(high - low) / (array[high]
 						      - array[low])) *
 			     (value - array[low]));
-		printf("Value checked array[%lu] is out of range\n", pos);
-	}
+	printf("Value checked array[%lu] is out of range\n", pos);
 
 	return (-1);
 }
