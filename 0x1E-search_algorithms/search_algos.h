c6b90b6d038c25c969d9419cc6d72960c4ab3c78
ade-mic <ademic.aina@gmail.com>
2024-03-13 10:19:40 +0100

    103-exponential

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,4 +6,5 @@ int binary_search(int *array, size_t size, int value);
 size_t min(size_t, size_t);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 #endif /* _SEARCH_ALGOS_H_ */
